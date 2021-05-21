03efb9439a289b65f342c48f2ac4f7f0bf10b6ab
abeaucha <adam.beauchamp@radiantsolutions.com>
2021-05-21 17:01:23 -0400

    format

--- __tests__/install-pypy.test.ts
+++ __tests__/install-pypy.test.ts
@@ -165,16 +165,16 @@ describe('installPyPy', () => {
     spyFsWriteFile.mockImplementation(() => undefined);
 
     spyHttpClient = jest.spyOn(HttpClient.prototype, 'getJson');
-    spyHttpClient.mockImplementation(
-      async (): Promise<ifm.ITypedResponse<IPyPyManifestRelease[]>> => {
-        const result = JSON.stringify(manifestData);
-        return {
-          statusCode: 200,
-          headers: {},
-          result: JSON.parse(result) as IPyPyManifestRelease[]
-        };
-      }
-    );
+    spyHttpClient.mockImplementation(async (): Promise<
+      ifm.ITypedResponse<IPyPyManifestRelease[]>
+    > => {
+      const result = JSON.stringify(manifestData);
+      return {
+        statusCode: 200,
+        headers: {},
+        result: JSON.parse(result) as IPyPyManifestRelease[]
+      };
+    });
 
     spyExec = jest.spyOn(exec, 'exec');
     spyExec.mockImplementation(() => undefined);
